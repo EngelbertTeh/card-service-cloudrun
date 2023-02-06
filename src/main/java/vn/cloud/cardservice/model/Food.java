51f5538f818c0656d3de544599424de256cdcff0
EngelbertTeh <engelbertteh.dev@gmail.com>
2023-02-06 18:47:01 +0800

    Updated

---
+++ src/main/java/vn/cloud/cardservice/model/Food.java
@@ -0,0 +1,18 @@
+package vn.cloud.cardservice.model;
+
+import javax.xml.stream.Location;
+
+public class Food {
+
+    private Long foodId;
+    private String title;
+    private String description;
+    private Double listDays;
+    private Boolean isPendingPickup;
+    private Boolean isCollected;
+    private Boolean isHalal;
+    private Boolean isListed;
+//    private Double longitude;
+//    private Double latitude;
+    private Location foodLocation;
+}
